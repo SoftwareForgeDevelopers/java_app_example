8c0329cd5a8a4b16eb47a5d8459ec9ef77b1c49b
FelipeArredond <pipechiqui77@gmail.com>
2024-01-18 22:58:39 -0500

    Added new function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,10 @@ public class Main {
         return a + b;
     }
 
+    public int subFunction(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
